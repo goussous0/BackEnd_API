896ae1b5eb8dfc16ba68681e49ff2bf8da2e43ba
goussous0 <73262976+goussous0@users.noreply.github.com>
2021-11-24 17:59:18 -0500

    Add files via upload

---
+++ config_.py
@@ -0,0 +1,35 @@
+from datetime import date, datetime
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+db = SQLAlchemy()
+
+
+class Config:
+
+    SECRET_KEY = ''.join(sample((ascii_letters + digits), 60))
+    uri = os.getenv("DATABASE_URL")
+    try:
+
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+    except BaseException:
+        uri = ''
+
+    SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///data_base.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FILES = '/'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'dev': DevelopmentConfig,
+    'prod': ProductionConfig,
+}
